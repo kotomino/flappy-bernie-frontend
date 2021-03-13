2576bf5e2e515b4b020d1e875d0b2cde3b5a47e6
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-03-13 15:54:15 -0500

    navigation bar content added

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,19 +1,10 @@
-function resetMain() {
-  mainDiv().innerHTML = "";
-}
+// const mainDiv = () => document.getElementById("main");
 
-function resetSideBar() {
-  sideDiv().innerHTML = "";
-}
+const resetMain = () => mainDiv().innerHTML = "";
 
-async function getGames() {
-  // fetch to rails api, games index. Then grab scores
-  // populate side div with the scores
-  const resp = await fetch(baseUrl +'/games')
-  const data = await resp.json();
-  games = data;
-  renderYourScores(data);
-}
+const resetSideBar = () => sideDiv().innerHTML = "";
+
+const resetStartDiv = () => startButtonDiv().innerHTML = "";
 
 function navbarTemplate() {
   return `
@@ -24,36 +15,8 @@ function navbarTemplate() {
       <li id="leaderboard"><a href="#"><i class="fas fa-medal"></i>Leaderboard</a></li>
       <li id="your-scores"><a href="#"><i class="fas fa-chart-bar"></i>Your Scores</a></li>
       <li id="info"><a href="#"><i class="fas fa-info-circle"></i>Info</a></li>
-      
   </ul> 
-  <div class="social_media">
-    <a href="#"><i class="fab fa-facebook-f"></i></a>
-    <a href="#"><i class="fab fa-twitter"></i></a>
-    <a href="#"><i class="fab fa-instagram"></i></a>
-  </div>
     `;
-}
-
-function startTemplate() {
-  return `
-  <div class="card" style="height: 530px">
-    <div class="card-body">
-      <p class="card-text">
-        <span class="badge rounded-pill bg-dark text-wrap">Please enjoy this game on a desktop computer using Chrome.</span>
-      </p>
-      <br>
-      <h5 class="card-title">Login to Play:</h5>
-      <p class="card-text">
-        <form id="form">
-          <div class="input-field">
-            <input type="text" name="name" id="name">
-          </div><br>
-          <input type="submit" value="Login" class="btn btn-primary">
-        </form>
-      </p>
-  </div>
-</div>
-  `;
 }
 
 function gameTemplate() {
@@ -72,7 +35,7 @@ function gameTemplate() {
 
 function scoreTemplate() {
   return `
-  <div class="card" style="height: 125px">
+  <div class="card border border-dark mb-3" style="height: 125px">
     <div class="card-body">
       <h1 class="card-title" id="current-score"></h1>
     </div>
@@ -82,35 +45,34 @@ function scoreTemplate() {
 
 function homeTemplate() {
   return `
-  <h3>Home</h3>
+  <div class="card border border-dark mb-3" style="height: 500px">
+    <div class="card-body">
+      <h3>Welcome, ${Game.all[Game.all.length -1].user.name}!</h3>
+      <p>Remember the oh-so addicating game, Flappy Bird? If you've ever wondered why the game had to be removed, you've come to the right place. We revived the game with none other than Bernard Sanders! </p>
+    </div>
+  </div>
   `;
 }
 
 function infoTemplate() {
   return `
-  <h3>Information</h3>
+  <div class="card border border-dark mb-3" style="height: 500px">
+    <div class="card-body">
+      <h3>Game Info</h3>
+      <p>This game was created by Kotomi Noguchi during her time as a Full-time Software Engineering student at Flatiron School. You can connect with her on <a href="https://github.com/kotomino">Github</a> or <a href=https://www.linkedin.com/in/kotomi-noguchi-04bb79a0/">LinkedIn</a>!</p>
+    </div>
+  </div>
   `;
 }
 
 function howToPlayTemplate() {
   return `
-  <h3>How To Play</h3>
-  `;
-}
-
-function topScoresTemplate() {
-  return `
-  <h3>Top Scores</h3>
-  <div id="scores">
-  </div
-  `;
-}
-
-function yourScoresTemplate() {
-  return `
-  <h3>Your High Scores</h3>
-  <div id="scores">
-  </div
+  <div class="card border border-dark mb-3" style="height: 500px">
+      <div class="card-body">
+        <h3>How To Play</h3>
+        <p>Press the spacebar to flap Bernie up and navigate him through the pipes. The pair of pipes have equally sized gaps placed at random heights. Bernie will descend automatically and the pipes' horizontal speed will remain constant. Each successful pass through a pair of pipes gains you one point. Colliding with a pipe or the ground ends the game. Happy flapping! </p>
+    </div>
+  </div>
   `;
 }
 
@@ -133,31 +95,9 @@ function startButtonEnabled() {
 function restartButton() {
   return `
   <div class="d-grid gap-2">
-    <button class="btn btn-dark" id="restart-btn" type="button">Restart Game</button>
+    <button class="btn btn-primary" id="restart-btn" type="button">Restart Game</button>
   </div
   `;
-}
-
-function renderStartPage() {
-  pipeCount = 0;
-  isGameOver = false;
-
-  resetMain();
-  resetSideBar();
-  mainDiv().innerHTML = gameTemplate();
-  sideDiv().innerHTML = startTemplate();
-  scoreDiv().innerHTML = scoreTemplate();
-  navbarDiv().innerHTML = navbarTemplate();
-
-  // have everything disabled until logged in
-  startButtonDiv().innerHTML = startButtonDisabled();
-  yourScoresNav().classList.add('disabled');
-  homeNav().classList.add('disabled');
-  howToPlayNav().classList.add('disabled');
-  leaderboardNav().classList.add('disabled');
-  infoNav().classList.add('disabled');
-
-  form().addEventListener('submit', submitName);
 }
 
 function renderHome() {
@@ -175,99 +115,42 @@ function renderInfo() {
   sideDiv().innerHTML = infoTemplate();
 }
 
-function renderTopScores() {
-  resetSideBar();
-  sideDiv().innerHTML = topScoresTemplate();
-
-  games.sort((a, b) => b.score - a.score);
-  
-  games.slice(0, 10).forEach(function(score) {
-    renderScore(score);
-  })
-}
-
-function renderYourScores() {
-  resetSideBar();
-  sideDiv().innerHTML = yourScoresTemplate();
-  
-  const name = games[games.length - 1].user.name;
-
-  yourGames = games.filter(function(game) {
-    return game.user.name == name;
-  })
-
-  yourGames.sort((a, b) => b.score - a.score);
-
-  yourGames.slice(0, 10).forEach(function(score) {
-    renderScore(score);
-  })
-}
-
-function renderScore(game) {
-  let div = document.createElement('div');
-  let p = document.createElement('p');
-  let topScores = document.getElementById('scores');
-
-  if (game.score <= 0) {
-      p.innerText = `${game.user.name}: 0`;
-    }
-  else if(game.score) {
-    p.innerText = `${game.user.name}: ${game.score}`;
-  } 
-  
-
-  div.appendChild(p);
-  topScores.appendChild(div);
-}
-
-async function submitName(e) {
-  e.preventDefault();
-
-  let strongParams = {
-    game: {
-      score: undefined,
-      user_attributes: nameInput().value
-    }
-  }
-
-  // send data to the backend via a post request
-  const resp = await fetch(baseUrl + '/games', {
-    body: JSON.stringify(strongParams),
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "POST"
-  })
-  const data = await resp.json();
-  
-  games.push(data);
-  users.push(data.user);
-
-  // enable nav buttons because user is logged in.
+function enableNav() {
   yourScoresNav().classList.remove('disabled');
   homeNav().classList.remove('disabled');
   howToPlayNav().classList.remove('disabled');
   leaderboardNav().classList.remove('disabled');
   infoNav().classList.remove('disabled');
 
-  startButtonDiv().innerHTML = startButtonEnabled();
-  const enabledBtn = document.getElementById('enabled-btn');
-  enabledBtn.addEventListener('click', renderGame);
-
-  getGames();
+  yourScoresNav().addEventListener('click', Game.renderYourScores);
+  leaderboardNav().addEventListener('click', Game.renderTopScores);
+  howToPlayNav().addEventListener('click', renderHowToPlay);
+  homeNav().addEventListener('click', renderHome);
+  infoNav().addEventListener('click', renderInfo);
 }
 
-function renderGame() {
-  
+function disableNav() {
   startButtonDiv().innerHTML = startButtonDisabled();
-  displayCurrentScore();
-  setTimeout(pipeCountPlusOne, 700);
+  yourScoresNav().classList.add('disabled');
+  homeNav().classList.add('disabled');
+  howToPlayNav().classList.add('disabled');
+  leaderboardNav().classList.add('disabled');
+  infoNav().classList.add('disabled');
   
-  generatePipe();
-  bernieDrops();
-  
-  document.addEventListener('keyup', jump);
+}
+
+function enableStartButton() {
+  resetStartDiv();
+  startButtonDiv().innerHTML = startButtonEnabled();
+  const enabledBtn = document.getElementById('enabled-btn');
+  enabledBtn.addEventListener('click', Game.renderGame);
+}
+
+function enableRestartButton() {
+  resetStartDiv();
+  startButtonDiv().innerHTML = restartButton();
+  let restartBtn = document.getElementById('restart-btn');
+  restartBtn.addEventListener('click', renderRestartGame);
 }
 
 async function renderRestartGame(e) {
@@ -275,6 +158,8 @@ async function renderRestartGame(e) {
   
   pipeCount = 0;
   isGameOver = false;
+  bernieLeft = 200; 
+  bernieBottom = 300; 
 
   resetMain();
   mainDiv().innerHTML = gameTemplate();
@@ -282,28 +167,15 @@ async function renderRestartGame(e) {
   let strongParams = {
     game: {
       score: undefined,
-      user_attributes: users[users.length - 1].name
+      user_attributes: User.all[User.all.length - 1].name
     }
   }
 
-   // send data to the backend via a post request
-   const resp = await fetch(baseUrl + '/games', {
-    body: JSON.stringify(strongParams),
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "POST"
-  })
-  const data = await resp.json();
-
-  games.push(data);
-
-  // startButtonDiv().innerHTML = startButtonDisabled();
-
-  getGames();
-  renderGame();
+  // send data to the backend via a post request
+  const data = await Api.post('/games', strongParams);
   
+  Game.getGamesRestart();
+  Game.renderGame();
 }
 
 function displayCurrentScore() {
@@ -312,7 +184,6 @@ function displayCurrentScore() {
   
   if (!isGameOver) {
     setTimeout(displayCurrentScore, 100);
-    console.log("display current score running");
   }
 }
 
@@ -346,96 +217,56 @@ function jump(e) {
 }
 
 function generatePipe() {
-    let pipeLeft = 500;
-    let pipeBottom = Math.random() * 150;
-    const pipe = document.createElement('div');
-    const topPipe = document.createElement('div');
-    if(!isGameOver) {
-      pipe.classList.add('pipe');
-      topPipe.classList.add('topPipe');
-    }
+  let pipeLeft = 500;
+  let pipeBottom = Math.random() * 150;
+  const pipe = document.createElement('div');
+  const topPipe = document.createElement('div');
+  if(!isGameOver) {
+    pipe.classList.add('pipe');
+    topPipe.classList.add('topPipe');
+  }
 
-    gameDisplay().appendChild(pipe);
-    gameDisplay().appendChild(topPipe);
+  gameDisplay().appendChild(pipe);
+  gameDisplay().appendChild(topPipe);
 
+  pipe.style.left = pipeLeft + 'px';
+  pipe.style.bottom = pipeBottom + 'px';
+  topPipe.style.left = pipeLeft + 'px';
+  topPipe.style.bottom = pipeBottom + gap + 'px';
+
+  function movePipe() {
+    pipeLeft -= 2;
     pipe.style.left = pipeLeft + 'px';
-    pipe.style.bottom = pipeBottom + 'px';
     topPipe.style.left = pipeLeft + 'px';
-    topPipe.style.bottom = pipeBottom + gap + 'px';
 
-    function movePipe() {
-      pipeLeft -= 2;
-      pipe.style.left = pipeLeft + 'px';
-      topPipe.style.left = pipeLeft + 'px';
-
-      if (pipeLeft === -60) {
-        clearInterval(movePipeTimerId);
-        gameDisplay().removeChild(pipe);
-        gameDisplay().removeChild(topPipe);
-      }
-      // (bernieBottom < pipeBottom + 145 || bernieBottom > pipeBottom + gap - 225 )
-      if ( pipeLeft > 160 && pipeLeft < 250 && (bernieBottom < pipeBottom + 145 || bernieBottom > pipeBottom + gap - 230) ) {
-        gameOver();
-      }
-      if(isGameOver) {
-        clearInterval(movePipeTimerId); // stops pipes when game over
-      }
+    if (pipeLeft === -60) {
+      clearInterval(movePipeTimerId);
+      gameDisplay().removeChild(pipe);
+      gameDisplay().removeChild(topPipe);
     }
-    let movePipeTimerId = setInterval(movePipe, 15); // calls movePipe every 20 miliseconds
-    if (!isGameOver) setTimeout(generatePipe, 2500) //generates pipe every 2.5 secs until game over
+    // (bernieBottom < pipeBottom + 145 || bernieBottom > pipeBottom + gap - 225 )
+    if ( pipeLeft > 160 && pipeLeft < 250 && (bernieBottom < pipeBottom + 145 || bernieBottom > pipeBottom + gap - 230) ) {
+      gameOver();
+    }
+    if(isGameOver) {
+      clearInterval(movePipeTimerId); // stops pipes when game over
+    }
+  }
+  let movePipeTimerId = setInterval(movePipe, 15); // calls movePipe every 20 miliseconds
+  if (!isGameOver) setTimeout(generatePipe, 2500) //generates pipe every 2.5 secs until game over
   }
 
   function gameOver() {
     isGameOver = true;
     document.removeEventListener('keyup', jump); // stop ability to jump
     console.log("Game over");
-    saveGameScore();
+    Game.saveGameScore(); 
 
-    // get ready for restart
-    startButtonDiv().innerHTML = restartButton();
-    let restartBtn = document.getElementById('restart-btn');
-    restartBtn.addEventListener('click', renderRestartGame);
-  }
-
-  function saveGameScore() {
-    let strongParams = {
-      game: {
-        score: pipeCount
-      }
-    }
-
-    const id = games[games.length - 1].id;
-  
-    fetch(baseUrl + '/games/' + id, {
-      method: "PATCH",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(strongParams)
-    })
-    .then(function(resp) {
-      return resp.json();
-    })
-    .then(function(game) {
-      let g = games.find(function(g) {
-        return g.id == game.id
-      })
-      let idx = games.indexOf(g);
-
-      games[idx] = game;
-
-      renderYourScores();
-    })    
+    setTimeout(enableRestartButton, 800); //restart button
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    renderStartPage();
-    yourScoresNav().addEventListener('click', renderYourScores);
-    leaderboardNav().addEventListener('click', renderTopScores);
-    howToPlayNav().addEventListener('click', renderHowToPlay);
-    homeNav().addEventListener('click', renderHome);
-    infoNav().addEventListener('click', renderInfo);
+    Game.renderStartPage();
   })   
 
 
